bb956b0f975cb926d1b477aac088684bf83d019d
Miguel <bydemon101@gmail.com>
2025-06-05 20:42:02 -0500

    Prueba 1.6

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch('https://wow-backend-teal.vercel.app/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
